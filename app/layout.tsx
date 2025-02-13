4ff6c25b8fac4069d473999d04c383d6d20cd4ec
carlos Rodriguez <carlos_rodriguez5@epam.com>
2025-02-13 13:03:25 -0500

    finishing the course

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css'
 import { inter } from './ui/fonts';
+import { Metadata } from 'next/dist/lib/metadata/types/metadata-interface';
+
+export const metadata: Metadata = {
+  title: {
+    template: '% | Acme Dashboard',
+    default: 'Acme Dashboard'
+  },
+  description: 'The official Next.js Course Dasboard, built with App router',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh')
+}
 
 export default function RootLayout({
   children,
---
+++ auth.ts
@@ -0,0 +1,43 @@
+import NextAuth from "next-auth";
+import { authConfig } from "./auth.config";
+import Credentials from "next-auth/providers/credentials";
+import { z } from "zod";
+import postgres from "postgres";
+import type { User } from "./app/lib/definitions";
+import bcrypt from "bcrypt";
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
+
+async function getUser(email: string): Promise<User | undefined> {
+  try {
+    const user = await sql<User[]>`SELECT * FROM users WHERE email=${email}`;
+    return user[0];
+  } catch (error) {
+    console.error("Failed to fetch user:", error);
+    throw new Error("Failed to fetch user.");
+  }
+}
+
+export const { auth, signIn, signOut } = NextAuth({
+  ...authConfig,
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCrendentials = z
+          .object({ email: z.string().email(), password: z.string().min(6) })
+          .safeParse(credentials);
+
+        if (parsedCrendentials.success) {
+          const { email, password } = parsedCrendentials.data;
+          const user = await getUser(email);
+          if (!user) return null;
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+
+          if (passwordsMatch) return user;
+        }
+        console.log('Invalid credentials');
+        return null;
+      },
+    }),
+  ],
+});
